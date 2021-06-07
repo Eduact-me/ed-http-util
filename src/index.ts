f44df06468ea22feb8b24bb64e0af0b5626fe437
Mohammed Essam <dev.mohammed.essam@gmial.com>
2021-06-07 13:47:35 +0200

    adding showMessage option with defaut value false in GET method , true otherwise

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ export default class Http {
     message: string = '',
     statusCode: number = 200,
     meta?: object,
+    showMessage?: boolean,
   ) {
+    showMessage = typeof showMessage === 'boolean' ? showMessage : responseObject.request.method !== 'GET';
     const responseBody = {
       meta,
       data,
       message,
+      showMessage,
     };
     return responseObject.status(statusCode).send(responseBody);
   }
